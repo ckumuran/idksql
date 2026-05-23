8c6588fd8846117b10e6a150b506d1ebb1bda2b7
kumaran <k31311558@gmail.com>
2026-05-23 11:27:30 +0530

    Create server.ts

---
+++ apps/server/src/server.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import app from "./app";
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+
+    console.log(`
+==================================
+IDKSQL SERVER RUNNING
+PORT: ${PORT}
+==================================
+`);
+});
