90fb5cf82120a91bf00973837da114e1afa0e962
kumaran <k31311558@gmail.com>
2026-05-23 11:19:32 +0530

    Create base.ts

---
+++ apps/server/src/adapters/base.ts
@@ -0,0 +1,10 @@
+export interface DatabaseAdapter {
+
+    connect(): Promise<void>;
+
+    disconnect(): Promise<void>;
+
+    query(sql: string): Promise<any[]>;
+
+    getSchema(): Promise<string>;
+}
